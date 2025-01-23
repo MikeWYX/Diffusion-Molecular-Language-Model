adbc63d21356eb429a10d7b797a3e12eb4b3bf10
lyc0930 <yc1023601986@mail.ustc.edu.cn>
2025-01-23 20:44:40 +0800

    update dataloader for zinc250k and natural product

--- dataloader.py
+++ dataloader.py
@@ -4,6 +4,7 @@ from functools import partial
 import torch
 from torch.nn.utils.rnn import pad_sequence
 from rdkit import Chem
+import json
 
 
 class DiffusionLoader:
@@ -51,155 +52,115 @@ class MoleculeLoader:
     def my_load(self, task_name, splits):
         return [self._load(task_name, name) for name in splits]
 
-class ConditionalLoader:
-    def __init__(self, tokenizer, return_source_length=False):
+class Zinc250kLoader:
+    def __init__(self, tokenizer):
         self.tokenizer = tokenizer
-        self.return_source_length = return_source_length
-        self.data_dir = './conditional_data'
-
-    @staticmethod
-    def _convert_to_features_original(example_batch, tokenizer):
-        q1 = tokenizer.batch_encode_plus(example_batch['src'], max_length=128, truncation=True, add_special_tokens=False)
-        q2 = tokenizer.batch_encode_plus(example_batch['trg'], max_length=128, truncation=True, add_special_tokens=False)
+        self.dataset = datasets.load_dataset('/NAS/luoyc/wuyux/data/zinc250k', split='train')
+        
+    def _get_splits(self):        
+        # Load test indices
+        selfies_list = self.dataset['selfies']
+        with open('/NAS/luoyc/wuyux/data/zinc250k/valid_idx_zinc250k.json') as f:
+            test_idx = np.array(json.load(f))    
+        # Calculate train indices
+        train_idx = np.array(list(set(np.arange(len(selfies_list))).difference(set(test_idx))))
+       
         return {
-            'source': q1['input_ids'],
-            'target': q2['input_ids'],
+            'train': train_idx.tolist(),
+            'test': test_idx.tolist()
         }
 
-    def load_original(self, split):
-        dataset = datasets.load_dataset(os.path.join(self.data_dir, self.task_name, f'{self.task_name}.py'), split=split)
-        dataset = dataset.map(partial(self._convert_to_features_original, tokenizer=self.tokenizer), batched=True, load_from_cache_file=False)
+    def _load_train(self):
+        # Get split indices
+        splits = self._get_splits()
+        split_idx = splits['train']
+        # Select subset for this split
+        dataset = self.dataset.select(split_idx)
+        
         print(f'Example in {split} set:')
         print(dataset[0])
+        
+        # Process features
+        removed_columns = ['index', 'smiles', 'selfies', 'logP', 'qed', 'SAS']
+        dataset = dataset.map(
+            partial(self.convert_to_features, tokenizer=self.tokenizer), 
+            batched=True, 
+            remove_columns=removed_columns
+        )
         return dataset
 
-    def _load(self, split):
-        dataset = datasets.load_dataset(os.path.join(self.data_dir, self.task_name, f'{self.task_name}.py'), split=split)
-        if self.return_source_length:
-            dataset = dataset.map(partial(self.add_original_src_length, tokenizer=self.tokenizer))
-        dataset = dataset.map(self.add_prompt)
-        dataset = dataset.map(partial(self.convert_to_features, tokenizer=self.tokenizer), batched=True)
-        print(f'Example in {split} set:')
-        print(dataset[0])
+    def _load_test(self):
+        splits = self._get_splits()
+        split_idx = splits['test']
+        dataset = self.dataset.select(split_idx)
         return dataset
-
-    def add_original_src_length(self, example, tokenizer):
-        return {
-            'original_src_length': len(tokenizer.encode(example['src'], max_length=128, truncation=True, add_special_tokens=False))
-        }
-
-    def my_load(self, splits):
-        return [self._load(name) for name in splits]
 
     @staticmethod
     def convert_to_features(example_batch, tokenizer):
-        q1 = tokenizer.batch_encode_plus(example_batch['src'], max_length=128, truncation=True, add_special_tokens=False)
-        q2 = tokenizer.batch_encode_plus(example_batch['trg'], max_length=128, truncation=True, add_special_tokens=False)
-        encodings = {
-            'source': q1['input_ids'],
-            'target': q2['input_ids'],
-        }
-
-        return encodings
-
-    @staticmethod
-    def collate_fn(batch_input, tokenizer):
-        input_ids = pad_sequence([torch.tensor(
-            [tokenizer.cls_token_id] + d['source'] + d['target'] + [tokenizer.sep_token_id]
-        ) for d in batch_input], batch_first=True)
-
-        attention_mask = torch.ones_like(input_ids)
-
-        target_mask = torch.stack([torch.cat([
-            torch.zeros(len(d['source']) + 1), torch.ones(input_ids.size(1) - len(d['source']) - 1)
-        ]) for d in batch_input])
-
-        assert input_ids.size() == attention_mask.size() == target_mask.size()
+        """Convert examples to features"""
+        input_encodings = tokenizer.batch_encode_plus(
+            example_batch['selfies'],
+            max_length=128,
+            truncation=True,
+            add_special_tokens=False
+        )
         return {
-            'input_ids': input_ids,
-            'attention_mask': attention_mask,
-            'target_mask': target_mask,
+            'input_ids': input_encodings['input_ids'],
+            'attention_mask': input_encodings['attention_mask'],
         }
 
-class QQPLoader(ConditionalLoader):
-    def __init__(self, tokenizer, return_source_length=False):
-        super(QQPLoader, self).__init__(tokenizer, return_source_length)
-        self.task_name = 'qqp'
+class NPLoader:
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+        
+    def _get_splits(self):        
+        # Load test indices
+        selfies_list = self.dataset['selfies']
+        with open('/NAS/luoyc/wuyux/data/zinc250k/valid_idx_zinc250k.json') as f:
+            test_idx = np.array(json.load(f))    
+        # Calculate train indices
+        train_idx = np.array(list(set(np.arange(len(selfies_list))).difference(set(test_idx))))
+       
+        return {
+            'train': train_idx.tolist(),
+            'test': test_idx.tolist()
+        }
 
-    @staticmethod
-    def add_prompt(example):
-        example['src'] = '"' + example['src'] + '" is equal to "'
-        example['trg'] = example['trg']
-        return example
-
-
-
-class QTLoader(ConditionalLoader):
-    def __init__(self, tokenizer, return_source_length=False):
-        super(QTLoader, self).__init__(tokenizer, return_source_length)
-        self.task_name = 'Q-T'
-
-    @staticmethod
-    def add_prompt(example):
-        example['src'] = ' Answer: ' + example['src'] + ' Question: '
-        example['trg'] = example['trg']
-        return example
-
-
-class WikiLoader(ConditionalLoader):
-    def __init__(self, tokenizer, return_source_length=False):
-        super(WikiLoader, self).__init__(tokenizer, return_source_length)
-        self.task_name = 'wiki_alignment'
-
-    @staticmethod
-    def add_prompt(example):
-        example['src'] = '"' + example['src'] + '" can be summarized as: '
-        example['trg'] = example['trg']
-        return example
-
-class CCLoader(ConditionalLoader):
-    def __init__(self, tokenizer, return_source_length=False):
-        super(CCLoader, self).__init__(tokenizer, return_source_length)
-        self.task_name = 'CC'
-
-    @staticmethod
-    def add_prompt(example):
-        example['src'] = example['src'] + ' - '
-        example['trg'] = example['trg']
-        return example
-
-
-class DiffusionLoaderWithElectra(DiffusionLoader):
-    def __init__(self, model_tokenizer, electra_tokenizer, electra_model):
-        super().__init__(model_tokenizer)
-        self.electra_tokenizer = electra_tokenizer
-        self.electra_model = electra_model
-
-    def _load(self, task_name, split):
-        dataset = datasets.load_dataset(f'./dataloaders/{task_name}.py', split=split)
+    def _load_train(self):
+        # Get split indices
+        splits = self._get_splits()
+        split_idx = splits['train']
+        # Select subset for this split
+        dataset = self.dataset.select(split_idx)
+        
         print(f'Example in {split} set:')
         print(dataset[0])
-        dataset = dataset.map(partial(self.new_convert_to_features, model_tokenizer=self.tokenizer, electra_tokenizer=self.electra_tokenizer, electra_model=self.electra_model), batched=True, remove_columns='text')
+        
+        # Process features
+        removed_columns = ['index', 'smiles', 'selfies', 'logP', 'qed', 'SAS']
+        dataset = dataset.map(
+            partial(self.convert_to_features, tokenizer=self.tokenizer), 
+            batched=True, 
+            remove_columns=removed_columns
+        )
+        return dataset
+
+    def _load_test(self):
+        splits = self._get_splits()
+        split_idx = splits['test']
+        dataset = self.dataset.select(split_idx)
         return dataset
 
     @staticmethod
-    def new_convert_to_features(example_batch, model_tokenizer, electra_tokenizer, electra_model):
-        input_encodings = model_tokenizer.batch_encode_plus(example_batch['text'], max_length=256, truncation=True, add_special_tokens=False)
-        electra_encodings = electra_tokenizer.batch_encode_plus(example_batch['text'], max_length=256, truncation=True, padding=True, return_tensors='pt', add_special_tokens=False)
-        for k in electra_encodings.keys():
-            electra_encodings[k] = electra_encodings[k].cuda()
-        position = electra_encodings['attention_mask'].count_nonzero(1)
-        with torch.no_grad():
-            logits = electra_model(**electra_encodings)
-
-
-        encodings = {
+    def convert_to_features(example_batch, tokenizer):
+        """Convert examples to features"""
+        input_encodings = tokenizer.batch_encode_plus(
+            example_batch['selfies'],
+            max_length=128,
+            truncation=True,
+            add_special_tokens=False
+        )
+        return {
             'input_ids': input_encodings['input_ids'],
             'attention_mask': input_encodings['attention_mask'],
-            'electra_logits': [logits[i][:position[i]] for i in range(position.size(0))]
         }
-
-        return encodings
-
-
-
