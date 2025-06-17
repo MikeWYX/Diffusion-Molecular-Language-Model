c091bca8dcc695e1426d55770ddc808d125d6aef
ustcwuyuxing <wuyuxing@mail.ustc.edu.cn>
2025-06-17 20:52:18 +0800

    Update token_freq.py

--- token_freq.py
+++ token_freq.py
@@ -1,5 +1,4 @@
 import os
-
 from transformers import AutoTokenizer
 import torch
 from dataloader import DiffusionLoader
@@ -8,21 +7,16 @@ import diffusion_word_freq
 import math
 from tqdm import tqdm
 
-# tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 tokenizer = AutoTokenizer.from_pretrained('/data/luoyc/MolGen-large')
 train_data = DiffusionLoader(tokenizer=tokenizer).my_load(task_name='zinc250k', splits=['train'])[0]
-
 token_freq = torch.zeros((len(tokenizer),), dtype=torch.int64)
 print(len(tokenizer))
 
-# for data in tqdm(train_data):
-#     for iid in data['input_ids']:
-#         token_freq[iid] += 1
+for data in tqdm(train_data):
+    for iid in data['input_ids']:
+        token_freq[iid] += 1
 
-# if not os.path.exists('./token_freq'):
-#     os.mkdir('token_freq')
+if not os.path.exists('./token_freq'):
+    os.mkdir('token_freq')
 
-# torch.save(token_freq, f'./token_freq/MolGen-large_zinc250k.pt')
-
-
-
+torch.save(token_freq, f'./token_freq/MolGen-large_zinc250k.pt')
